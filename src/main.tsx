26a0ed5c9046aaf00acfbc1fd347d409a247b641
sean.chen@slalom.com <sean.chen@slalom.com>
2026-09-21 15:39:03 -0700

    Set up app and add Vuetify dependency config

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
 import './index.css'
 import App from './App.tsx'
 
@@ -8,3 +10,4 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>,
 )
+
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import vuetify from 'vite-plugin-vuetify'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vuetify({ autoImport: true })],
 })
