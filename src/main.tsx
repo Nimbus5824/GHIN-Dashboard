47eaf6006433f18d795794e58066a6fe6c741ce3
sean.chen@slalom.com <sean.chen@slalom.com>
2026-09-21 15:49:15 -0700

    Remove incompatible Vuetify configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'vuetify/styles'
-import '@mdi/font/css/materialdesignicons.css'
 import './index.css'
 import App from './App.tsx'
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import vuetify from 'vite-plugin-vuetify'
 
 export default defineConfig({
-  plugins: [react(), vuetify({ autoImport: true })],
+  plugins: [react()],
 })
